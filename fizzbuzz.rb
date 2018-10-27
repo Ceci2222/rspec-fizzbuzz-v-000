e2174047e37050de903a2fa7a91e9b7d6f23b8b8
Marie Burns <mcburns2222@gmail.com>
2018-10-27 17:34:31 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,13 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if (int % 3 == 0) && (int % 5 == 0)
-    print "FizzBuzz"
+    return "FizzBuzz"
   
   elsif int % 3 == 0
-    print "Fizz"
+    return "Fizz"
   
   elsif int % 5 == 0 
-    print "Buzz"
+    return "Buzz"
   else
   end
 end
